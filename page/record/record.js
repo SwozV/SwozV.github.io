163765ee80f2e4eb0018046ba9e8df24b5b97548
Kyon <swoz.08.v@outlook.com>
2024-02-27 23:22:06 +0800

    Re-Edit

--- page/record/record.js
+++ page/record/record.js
@@ -1,15 +1,25 @@
-fetch("./records.json")
+fetch("records.json")
   .then((response) => response.json())
   .then((data) => {
-    const myData = JSON.parse(JSON.stringify(data));
     const table = document.getElementById("records");
-    const tableHeader = table.createTHead();
-    const headerRow = tableHeader.insertRow();
-    headerRow.innerHTML =
-      "<th>Time</th><th>Name</th><th>Type</th><th>Score</th>";
-    for (let i = 0; i < myData.length; i++) {
-      const dataRow = table.insertRow();
-      dataRow.innerHTML = `<td>${myData[i].time}</td><td>${myData[i].name}</td><td>${myData[i].class}</td><td>${myData[i].score}</td>`;
+
+    // 创建表头
+    const thead = table.createTHead();
+    const headerRow = thead.insertRow();
+    for (let key in data[0]) {
+      const headerCell = document.createElement("th");
+      headerCell.textContent = key;
+      headerRow.appendChild(headerCell);
     }
+
+    // 创建表格内容
+    const tbody = table.createTBody();
+    data.forEach((item) => {
+      const row = tbody.insertRow();
+      for (let key in item) {
+        const cell = row.insertCell();
+        cell.textContent = item[key];
+      }
+    });
   })
-  .catch((error) => console.error(error));
+  .catch((error) => console.error("Error:", error));
